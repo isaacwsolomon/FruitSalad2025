7bd427b6fbad9e661deca6e887a91b3fbe9a37db
Isaac <isaacwsolomon@gmail.com>
2025-05-04 18:34:23 +0200

    Random code generated when user goes to create-game page

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.set('view engine', 'ejs') // after app variable
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const { createRandomString } = require('./create-game');
+
 // Route for the home page
 app.get("/", (req, res) => {
     res.render("index"); // Render the index.ejs view
@@ -19,7 +21,8 @@ app.get("/join-game", (req, res) => {
 
 // Route for create game page
 app.get("/create-game", (req, res) => {
-    res.render("create-game");
+    const gameCode = createRandomString(5)
+    res.render("create-game", {gameCode});
 });
 // Start server on 3004 
 app.listen(3004, () => {
